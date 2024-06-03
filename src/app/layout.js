8271e3ee0613581dc9804ebbb55dda6d82f7a361
mosalehkamali <mosalehkamali@gmail.com>
2024-06-04 01:06:33 +0330

    create button for scroll to top

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
+import ScrollToTop from "@/components/modules/scrollButton/ScrollToTop";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa">
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body suppressHydrationWarning={true}>
+        {children}
+        <ScrollToTop />
+        </body>
     </html>
   );
 }
