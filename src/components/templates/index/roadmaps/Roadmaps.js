565e83b3ea22f3dd3d5e115914c0347da5af38f5
mosalehkamali <mosalehkamali@gmail.com>
2024-06-05 00:35:59 +0330

    "Added swiper dependency and implemented swiper component in Roadmaps.js"

--- src/components/templates/index/roadmaps/Roadmaps.js
+++ src/components/templates/index/roadmaps/Roadmaps.js
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useState } from "react";
 import styles from "./Roadmaps.module.css";
 import { PiArrowLeftFill, PiCirclesThreeFill } from "react-icons/pi";
 import Link from "next/link";
 import Road from "@/components/modules/cards/Road";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
 function Roadmaps() {
   return (
     <div className={styles.roadmaps}>
@@ -16,7 +28,48 @@ function Roadmaps() {
           <PiArrowLeftFill />
         </Link>
       </div>
-      <Road />
+
+      <div className={styles.swiperContainer}>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Road />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Road />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Road />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Road />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Road />
+        </SwiperSlide>
+      </Swiper>
+      </div>
     </div>
   );
 }
