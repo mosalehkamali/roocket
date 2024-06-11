482cda58c46aff50dafbc8a00607cddd3cc97978
mosalehkamali <mosalehkamali@gmail.com>
2024-06-11 10:55:16 +0330

    create not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,19 @@
+import styles from "@/styles/404.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      <div className={styles.contents}>
+        <Image width={500} height={500} src="/assets/404-error.svg"></Image>
+      </div>
+      <div className={styles.texts}>
+        <p>صفحه مورد نظر یافت نشد :((</p>
+        <Link href="/">برگشت به صفحه اصلی</Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;
