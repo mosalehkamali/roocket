8736eae9f481d0b23b6d98c1f426909ee1a2fd85
mosalehkamali <mosalehkamali@gmail.com>
2024-06-29 17:05:08 +0330

    update series page

--- src/app/series/page.js
+++ src/app/series/page.js
@@ -1,13 +1,16 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Courses from "@/components/templates/series/courses/Courses";
 import Roadmaps from "@/components/templates/index/roadmaps/Roadmaps";
 import React from "react";
+import Footer from "@/components/modules/footer/Footer";
 
 function series() {
   return (
     <>
       <Navbar />
       <Roadmaps />
-      
+      <Courses/>
+      <Footer/>
     </>
   );
 }
