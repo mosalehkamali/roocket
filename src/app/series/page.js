9c9454baceda0fcf65bce5966f7f6c3fac7c61ad
mosalehkamali <mosalehkamali@gmail.com>
2024-06-29 15:59:39 +0330

    create series page

---
+++ src/app/series/page.js
@@ -0,0 +1,15 @@
+import Navbar from "@/components/modules/navbar/Navbar";
+import Roadmaps from "@/components/templates/index/roadmaps/Roadmaps";
+import React from "react";
+
+function series() {
+  return (
+    <>
+      <Navbar />
+      <Roadmaps />
+      
+    </>
+  );
+}
+
+export default series;
